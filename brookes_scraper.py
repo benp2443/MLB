c0988d62fbd65ac97214a6c7b33016c58ad2333a
benp2443 <benpaul374@gmail.com>
2018-03-24 10:19:36 +1100

    WIP

---
+++ brookes_scraper.py
@@ -0,0 +1,133 @@
+import pandas as pd
+import numpy as np
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select, WebDriverWait
+import sys
+import time
+
+def get_pitcher_links(): #pitcher_id_list):
+        pitcher_tab = driver.find_element_by_name('pitchSel')
+        pitchers = pitcher_tab.find_elements_by_tag_name('option')
+
+        if len(pitchers) == 0:
+                return []
+
+        pitcher_list = []
+
+        for pitcher in pitchers:
+                pitcher_id = pitcher.get_attribute('value')
+                pitcher_list.append(pitcher_id)
+                #if pitcher_id in pitch_list:  *********ADD BACK WHEN PITCHERS IN DATA ARE KNOWN**********
+                #        pitcher_list.append(pitcher_id_list)
+
+        day_links = []
+        for pitcher in pitcher_list:
+                print(pitcher)        
+                select = Select(driver.find_element_by_name('pitchSel'))
+                select.select_by_value(pitcher)
+                
+                try:
+                    button = driver.find_element_by_css_selector("input[value = 'Select']")
+                    button.click()
+                except:
+                    print('No Button')
+                    return day_links
+                
+                link_button = driver.find_element_by_xpath('//*[@id="top"]/div[3]/div[2]/div/div[2]/div[2]/p[1]/a')
+                day_links.append(link_button.get_attribute('href'))
+
+        sys.stdout.flush()
+        return day_links
+
+
+def game_data(link):
+
+        driver.get(link)
+
+        soup = BeautifulSoup(driver.page_source, 'lxml')
+        pitch_count = 0
+        info = []
+        table = soup.find('table')
+        for tr in table.find_all('tr'):
+                if tr.find('td') == None:
+                        continue
+                else:
+                        data = tr.find_all('td')
+
+                        pitcher_id = data[5].text
+                        pitch_type = data[15].text
+                        game_id = data[22].text
+
+                        info.append([pitcher_id, pitch_type, game_id, pitch_count])
+
+                        pitch_count += 1
+
+        temp = pd.DataFrame(info, columns = ['pitcher_id', 'pitch_type', 'game_id', 'pitch_count'])
+
+        return temp
+
+driver = webdriver.PhantomJS(r'/usr/bin/phantomjs')
+
+years = [2017]
+months = range(3,4) # All month tabs go from March to December
+days = range(29,32) # All day tabs go from 1 to 31
+
+#pitcher_id_list = [] -> List of pitchers id's to scrap. Parsed into pitcher_list function
+links = []
+
+for year in years:
+        for month in months:
+                if month == 3 or month == 12:
+                        continue
+                for day in days:
+
+                        url = 'http://www.brooksbaseball.net/pfxVB/pfx.php?month={0}&day={1}&year={2}&prevDate=314&league=mlb'.format(month, day, year)
+                        print(url)
+                        sys.stdout.flush()
+                        driver.get(url)
+
+                        game_tab = driver.find_element_by_name('game')
+                        games = game_tab.find_elements_by_tag_name('option')
+
+                        if len(games) == 0:
+                                continue
+
+                        games_list = []
+
+                        for game in games:
+                                games_list.append(game.get_attribute('value'))
+
+                        for game in games_list:
+                                
+                                select = Select(driver.find_element_by_name('game'))
+                                select.select_by_value(game)
+
+                                try:
+                                        button = driver.find_element_by_css_selector("input[value = 'Select']")
+                                except:
+                                        print('No Buttom')
+                                        continue
+
+                                button.click()
+
+                                game_links = get_pitcher_links() #(pitcher_id_list):
+
+                                if len(game_links) == 0:
+                                        continue
+
+                                links += game_links
+
+df = pd.DataFrame()
+
+for link in links:
+
+        temp = game_data(link)
+
+        if len(df) == 0:
+                df = temp
+        else:
+                df = pd.concat([df, temp], axis = 0)
+
+df.to_csv('brookes2017.csv', index = False)
--- cleaning.py
+++ cleaning.py
@@ -5,17 +5,32 @@ df = pd.read_csv('filtered_pitchers.csv')
 
 print(df.columns.values)
 
-columns = ['ab_event', 'outs', 'home_runs', 'away_runs', 'p_description', 'type']
+df['home_runs'].replace(-1, np.nan, inplace = True)
+df['away_runs'].replace(-1, np.nan, inplace = True)
 
-for column in columns:
-    print(df[column].unique())
+games = df.loc[df['home_runs'].isnull(), 'game_id'].drop_duplicates().tolist()
+print(games)
+temp = df.loc[df['game_id'] == games[0], :]
+temp_idx = temp.index.values
+start_idx = temp_idx[0]
+end_idx = temp_idx[-1]
+
+start_value = temp.loc[start_idx, 'home_runs']
+
+if start_value == None:
+    temp.loc[start_idx, 'home_runs'] = 0
+
+temp['home_runs'].fillna(method = 'ffill', inplace = True)
+df.loc[temp_idx, 'home_runs'] = temp['home_runs']
+
+print(len(df.loc[(df['home_runs'].isnull()) & (df['game_id'] == games[0]), :]))
 
 ## Remove all star games
 #df = df.loc[df['season_period'] != 'A', :] 
 #
 ## Drop unwanted columns
 #drop_columns = ['ab_start_date_time', 'ab_end_date_time', 'ab_event_number', 'season_period', 'league_home', 'league_away', \
-#        'cc', 'mt', 'id_', 'code', 'location', 'nasty', 'ab_event_number', 'p_time', 'x', 'y']
+#        'cc', 'mt', 'id_', 'code', 'location', 'nasty', 'ab_event_number', 'p_time', 'x', 'y', 'balls', 'strikes', 'event_num' ]
 #
 #df = df.drop(drop_columns, axis = 1)
 #
