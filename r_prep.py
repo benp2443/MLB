75c609e95702843a1494c877a7e109fc22bc33c0
benp2443 <benpaul374@gmail.com>
2018-03-24 14:22:19 +1100

    new visuals

---
+++ r_prep.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('filtered_pitchers.csv')
+
+# Output csv with mean pitch type confidence for each pitcher
+pitch_conf_df = df.groupby('pitcher_id')['type_confidence'].mean().reset_index()
+pitch_conf_df.to_csv('type_confidence.csv', index = False)
+
+# Randomly selected 4 pitchers in the bottom/top 10% of mean pitch type confidence
+pitch_conf_df.sort_values(by = ['type_confidence'], inplace = True)
+
+bot_10_percent = pitch_conf_df.iloc[0:18, :] # ~ 180 pitchers in data. Hence first 18 rows are in bottome 10 percent
+bot_sample = bot_10_percent.sample(4)
+
+top_10_percent = pitch_conf_df.iloc[-18:, :]
+top_sample = top_10_percent.sample(4)
+
+# With the sample pitchers, find the mean confidence for each of there pitch types
+pitchers = bot_sample['pitcher_id'].tolist() + top_sample['pitcher_id'].tolist()
+
+temp = df.loc[df['pitcher_id'].isin(pitchers), ['pitcher_id', 'pitch_type', 'type_confidence']]
+conf_by_pitch = temp.groupby(['pitcher_id', 'pitch_type']).mean().reset_index()
+
+# Create new df which only includes pitch types which are thrown with more than 5% frequency (individually for each pitcher)
+pitch_counts = temp.groupby(['pitcher_id', 'pitch_type']).count().reset_index()
+pitch_counts.columns = ['pitcher_id', 'pitch_type', 'count']
+
+final = pd.DataFrame()
+for pitcher in pitchers:
+    counts = pitch_counts.loc[pitch_counts['pitcher_id'] == pitcher, :]
+    pitchers_thrown = float(counts['count'].sum())
+
+    keep_pitch_types = []
+    i = 0
+    while i < len(counts):
+        pitch_type = counts.iloc[i, 1]
+        pitch_count = float(counts.iloc[i, 2])
+        pitch_percent = pitch_count/pitchers_thrown
+
+        if pitch_percent > 0.05:
+            keep_pitch_types.append(pitch_type)
+
+        i += 1
+
+    temp2 = conf_by_pitch.loc[(conf_by_pitch['pitcher_id'] == pitcher) & (conf_by_pitch['pitch_type'].isin(keep_pitch_types)), :]
+
+    if len(final) == 0:
+        final = temp2
+    else:
+        final = pd.concat([final, temp2])
+
+# Add column to final df which says if pitch from top or bottom and save final csv
+bot_pitchers = bot_sample['pitcher_id'].drop_duplicates().tolist()
+final['class'] = ['bottom' if x in bot_pitchers else 'top' for x in final['pitcher_id']]
+
+final.to_csv('per_pitch_confidence.csv', index = False)
--- r_visual.R
+++ r_visual.R
@@ -1,7 +1,14 @@
 library('ggplot2')
 
-df <- read.csv('type_confidence.csv', header = FALSE, col.names = 'mean_type_confidence')
+df <- read.csv('type_confidence.csv', header = TRUE)
 
-ggplot(df, aes(x = mean_type_confidence)) +
+ggplot(df, aes(x = type_confidence)) +
 	geom_density()
 ggsave(filename = 'type_confidence_density.pdf')
+
+df <- read.csv('per_pitch_confidence.csv')
+df <- subset(df, pitcher_id == 462382)
+
+ggplot(df, aes(x = pitch_type, y = type_confidence)) +
+       geom_bar(stat = 'identity', fill = 'steelblue')
+ggsave(filename = 'bar.pdf')	
