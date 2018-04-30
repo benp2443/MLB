250cb6e360e5637b086bcb35b134b1d080a67d62
Top Dog <benpaul374@gmail.com>
2018-05-01 09:42:51 +1000

    WIP

--- pitchType_cluster.py
+++ pitchType_cluster.py
@@ -1,77 +1,97 @@
 import pandas as pd
 import numpy as np
+import sys
+import argparse
 from sklearn.cluster import KMeans
 from sklearn import preprocessing
+from sklearn import metrics
+from sklearn.metrics import pairwise_distances
 
-def cluster_pitch_types(df, pitcher_id):
-    df2 = pd.read_csv(df)
+pd.set_option('display.max_columns', 20)
+pd.set_option('display.max_rows', 300)
 
-    temp = df2.loc[df2['pitcher_id'] == pitcher_id, ['pfx_x', 'pfx_y', 'start_speed', 'pitch_type']]
+parser = argparse.ArgumentParser()
+parser.add_argument('--input', nargs = '+', help = 'input data path')
+args = parser.parse_args()
+
+def cluster_pitch_types(df_path):
+    df2 = pd.read_csv(df_path)
+
+    temp = df2.loc[df2['year'] != 2014, ['pfx_x', 'pfx_y', 'start_speed', 'pitch_type']]
     temp.dropna(axis = 0, how = 'any', inplace = True)
     number_clusters = len(temp['pitch_type'].unique())
+    X_scaled = preprocessing.scale(temp.iloc[:, 0:-1].values)
 
-    kmeans = KMeans(n_clusters = number_clusters, n_init = 20, random_state = 0)
+    cluster_labels = []
+    scores = []
+    for i in range(2, number_clusters + 1):
+        kmeans = KMeans(n_clusters = i, n_init = 20, random_state = 0).fit(X_scaled)
+        labels = kmeans.labels_.tolist()
 
-    X_scaled = preprocessing.scale(temp.iloc[:, 0:-1].values) 
-    clusters = kmeans.fit_predict(X_scaled)
+        cluster_labels.append(labels)
+        scores.append(metrics.calinski_harabaz_score(X_scaled, labels))
 
-    temp['cluster'] = kmeans.labels_
+    max_idx = scores.index(max(scores))
+    if max_idx + 1 == len(scores):
+        temp['cluster'] = cluster_labels[max_idx]
+    else:
+        temp['cluster'] = cluster_labels[max_idx+1]
 
     results = temp.groupby(['cluster', 'pitch_type'])['pfx_x'].count().reset_index()
     results.rename(columns = {'pfx_x':'count'}, inplace = True)
     print(results)
+    
+    pitch_types = results['pitch_type'].unique()
+    pitch_grouping = {}
+    for pitch in pitch_types:
+        pitch_grouping[pitch] = {'cluster':-1, 'count':-1, 'idx':-1}
 
-    pitch_types = []
-    for i in range(0, number_clusters):
-        temp2 = results.loc[results['cluster'] == i, :].reset_index(drop = True)
+    i = 0
+    while i < len(results):
+        pitch_type = results.iloc[i, 1]
+        count = results.iloc[i, 2]
+        
+        if count > pitch_grouping[pitch_type]['count']:  # What if count is equal -> which cluster to assign to?
+            cluster = results.iloc[i,0]
+            pitch_grouping[pitch_type]['cluster'] = cluster
+            pitch_grouping[pitch_type]['count'] = count
+            pitch_grouping[pitch_type]['idx'] = i
 
-        length = float(len(temp2))
-        cluster_total = float(temp2['count'].sum())
+        i += 1
+    
+    idx_list = []
+    for pitch in pitch_types:
+        idx_list.append(pitch_grouping[pitch]['idx'])
 
-        two_pitch_thresehold = 0.3
-        other_pitch_thresehold = 1/length
+    results2 = results.iloc[idx_list, :].reset_index(drop = True).sort_values(by = 'pitch_type', axis = 0)
 
-        aa = []
-        j = 0
-        while j < length:
-            pitch_type = temp2.iloc[j, 1]
-            pitch_count = float(temp2.iloc[j, 2])
-            pitch_percent = pitch_count/cluster_total
+    number_of_clusters = results2['cluster'].unique()
 
-            if length <= 3:
-                if pitch_percent > two_pitch_thresehold:
-                    aa.append(pitch_type)
-            else:
-                if pitch_percent > other_pitch_thresehold:
-                    aa.append(pitch_type)
+    mapper = {}
+    for cluster in number_of_clusters:
+        mapper[cluster] = ''
 
-            j += 1
+    i = 0
+    while i < len(results2):
+        cluster = results2.iat[i, 0]
+        pitch_type = results2.iat[i, 1]
+        if mapper[cluster] == '':
+            mapper[cluster] = pitch_type
+        else:
+            mapper[cluster] += '-' + pitch_type
 
-        pitch_types.append(aa)
+        i += 1
+                
+    results2['group'] = results2['cluster'].map(mapper)
 
-    return pitch_types
+    final_mapper = dict(zip(results2['pitch_type'], results2['group']))
 
-test1 = cluster_pitch_types('mlb_gd_full.csv', 465657) 
-print(test1)
-test2 = cluster_pitch_types('mlb_gd_full.csv', 593372)
-print(test2)
-test3 = cluster_pitch_types('mlb_gd_full.csv', 434378)
-print(test3)
+    df2['group_pitch_type'] = df2['pitch_type'].map(final_mapper)
+    
+    return df2
 
-#clusters_465657 = {'FF':'FF/FT/FC', 'FT':'FF/FT/FC', 'FC':'FF/FT/FC', 'CU':'CU', 'SL':'SL', 'CH':'CH'}
-#clusters_593372 = {'CH':'CH', 'FF':'FF/FT', 'FT':'FF/FT', 'CU':'CU/SL', 'SL':'CU/SL'}
-
-#pitchers = [465657, 593372]
-
-#temp = pd.read_csv('per_pitch_confidence.csv')
-#temp1 = temp.loc[temp['pitcher_id'] == pitchers[0], :]
-#temp2 = temp.loc[temp['pitcher_id'] == pitchers[1], :]
-
-#temp1['cluster'] = temp1['pitch_type'].map(clusters_465657)
-#temp2['cluster'] = temp2['pitch_type'].map(clusters_593372)
-
-#final = pd.concat([temp1, temp2], axis = 0)
-#print(final.head(10))
-#print(final.tail(10))
-
-#final.to_csv('delete.csv', index = False)
+for pitcher in args.input:
+    print(pitcher)
+    sys.stdout.flush()
+    new_df = cluster_pitch_types(pitcher)
+    new_df.to_csv(pitcher)
